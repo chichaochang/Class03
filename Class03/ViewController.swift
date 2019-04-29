91413f68fc9f4e872262b0944d24555cfb5e8a48
Chi-Chao Chang <chichao.chang@gmail.com>
2019-04-29 11:46:48 +0800

    New ViewController

--- Class03/ViewController.swift
+++ Class03/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbmsg: UILabel!
+    @IBAction func btnpushclicked(_ sender: Any) {
+        lbmsg.text = "Hi, XCode"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
